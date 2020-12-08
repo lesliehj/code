d9057d7ec0b25be6307050a34ada4ebe5802296b
十二少 <1009573788@qq.com>
2020-12-08 20:40:48 +0800

    code

---
+++ src/main/java/Code.java
@@ -0,0 +1,10 @@
+/**
+ * @author hj
+ * @description: TODO
+ * @date 2020/12/8 20:40
+ */
+public class Code {
+    public static void main(String[] args) {
+        System.out.println(666);
+    }
+}
